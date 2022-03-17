6d7912c5b96e064187c14f44bed06d71ea02c364
Иван Агабалаев <agabalaev01@yandex.ru>
2022-03-17 13:18:03 +0300

    Add entities for user, bank account and transaction history unit
    Creating entities in a Core project is necessary to use a common contract in all projects.

---
+++ Minibank.Core/Domains/BankAccounts/BankAccount.cs
@@ -0,0 +1,13 @@
+﻿namespace Minibank.Core.Domains.BankAccounts
+{
+    internal class BankAccount
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public double AccountBalance { get; set; }
+        public string Currency { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime OpeningDate { get; set; }
+        public DateTime ClosingDate { get; set; }
+    }
+}
