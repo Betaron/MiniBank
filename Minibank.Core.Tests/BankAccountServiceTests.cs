fcd88bdedfef0d8393511b43c61489c2333ef0a4
Иван Агабалаев <agabalaev01@yandex.ru>
2022-04-16 15:38:13 +0300

    Move the test project to the Tests folder at the solution level
    Add the nuGet Moq package to create fake repositories in the future.
    Add separate classes to write tests for user and account

---
+++ Minibank.Core.Tests/BankAccountServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Minibank.Core.Domains.BankAccounts.Services;
+using Xunit;
+using Moq;
+
+namespace Minibank.Core.Tests
+{
+    public class BankAccountServiceTests
+    {
+        private readonly IBankAccountService _bankAccountService;
+
+        public BankAccountServiceTests(IBankAccountService bankAccountService)
+        {
+            _bankAccountService = bankAccountService;
+        }
+    }
+}
---
+++ Minibank.Core.Tests/UserServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Minibank.Core.Domains.Users.Services;
+using Xunit;
+using Moq;
+
+namespace Minibank.Core.Tests
+{
+    public class UserServiceTests
+    {
+        private readonly IUserService _userService;
+        
+        public UserServiceTests(IUserService userService)
+        {
+            _userService = userService;
+        }
+    }
+}
